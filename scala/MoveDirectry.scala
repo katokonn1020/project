a4d53365752baef183642eb5579ca993a4d6060d
katoseiji <seiji@katoseiji-no-MacBook-Pro.local>
2017-06-17 05:27:21 +0900

    git remote -v で取得したstringからcase文でmkdirを分岐させる直前

--- scala/MoveDirectry.scala
+++ scala/MoveDirectry.scala
@@ -3,12 +3,42 @@ import java.io.File
 
 object MoveDirectry {
   def main(args: Array[String]): Unit = {
-    //val process = Process("mkdir sample").run
-    //val currentPath = new File(".").getAbsoluteFile.getParent
-    //val process = Process("git remote -v").lines.toList
-    //Process(Seq("cd","../","mkdir","sample"), new File(currentPath)).!
-    val dirList = Process(Seq("ls"), new File("/Users/seiji/git/project2/project/before/src")).lines.toList
-    println(dirList.toList(0))
-    //val process = Process("git remote -v").lines.toList
+    val currentPath = new File(".").getAbsoluteFile.getParent
+    val dirList = Process(Seq("sh","-c","cd ../before/src && ls")).lines.toList
+    dirList.foreach(mkdir)
+  }
+ 
+  def mkdir(name: String){
+    //死んでる変数ですが、どうやっても""の中に変数を入れる方法が見つからなかったので、一旦stringに格納
+    val gitPlainText = "git -C ../before/src/"+name+" remote -v"
+    val gitText = Process(Seq("sh","-c",gitPlainText)).lines.toList(0)
+println(gitText)
+
+    val githubWith = "(.*github.com.*)".r
+    val googleWith = "(.*source.developers.google.com.*)".r
+    val awsWith = "(.*git-codecommit.us-east-1.amazonaws.com.*)".r
+    val dirText =
+      gitText match {
+        case githubWith(matched) => gitText.split("github.com") 
+        case _ => println("Unmatched!")
+      }
+      gitText match {
+        case googleWith(matched) => println("this is google") // "abcdefg"
+        case _ => println("Unmatched!")
+      }
+      gitText match {
+        case awsWith(matched) => println("this is aws") // "abcdefg"
+        case _ => println("Unmatched!")
+      }
+
+
+
+
+
+
+
+
+
+    //Process(Seq("sh","-c","cd ../before/src && mkdir "))
   }
 }
