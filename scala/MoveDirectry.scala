8c53077dc73a89e3384fccfdca167cb89844b21a
katoseiji <seiji@katoseiji-no-MacBook-Pro.local>
2017-06-17 19:07:00 +0900

    それぞれのgit remote -vを取得できるようになったところ

--- scala/MoveDirectry.scala
+++ scala/MoveDirectry.scala
@@ -12,33 +12,28 @@ object MoveDirectry {
     //死んでる変数ですが、どうやっても""の中に変数を入れる方法が見つからなかったので、一旦stringに格納
     val gitPlainText = "git -C ../before/src/"+name+" remote -v"
     val gitText = Process(Seq("sh","-c",gitPlainText)).lines.toList(0)
-println(gitText)
 
+    //リポジトリ名を先に記しておかないといけない仕様
     val githubWith = "(.*github.com.*)".r
     val googleWith = "(.*source.developers.google.com.*)".r
     val awsWith = "(.*git-codecommit.us-east-1.amazonaws.com.*)".r
-    val dirText =
+
       gitText match {
-        case githubWith(matched) => gitText.split("github.com") 
-        case _ => println("Unmatched!")
+        case githubWith(matched) => 
+         gitText.split("github.com.").last.split("/").init.foreach()
+       
+        case _ =>
       }
       gitText match {
-        case googleWith(matched) => println("this is google") // "abcdefg"
-        case _ => println("Unmatched!")
+        case googleWith(matched) => 
+          //println(gitText.split("source.developers.google.com.")(1))
+        case _ =>
       }
       gitText match {
-        case awsWith(matched) => println("this is aws") // "abcdefg"
-        case _ => println("Unmatched!")
+        case awsWith(matched) =>
+          //println(gitText.split("git-codecommit.us-east-1.amazonaws.com.")(1))
+        case _ =>
       }
 
-
-
-
-
-
-
-
-
-    //Process(Seq("sh","-c","cd ../before/src && mkdir "))
   }
 }
