944ca037a27344137f76a996b19692fc346849ff
katoseiji <seiji@katoseiji-no-MacBook-Pro.local>
2017-06-21 04:13:36 +0900

    mkdir機能だけ完成

--- scala/MoveDirectry.scala
+++ scala/MoveDirectry.scala
@@ -1,39 +1,53 @@
 import scala.sys.process._
 import java.io.File
 
+import scala.collection.immutable
+
 object MoveDirectry {
   def main(args: Array[String]): Unit = {
     val currentPath = new File(".").getAbsoluteFile.getParent
-    val dirList = Process(Seq("sh","-c","cd ../before/src && ls")).lines.toList
+    val dirList     = Process(Seq("sh","-c","cd ../before/src && ls")).lines.toList
     dirList.foreach(mkdir)
   }
- 
-  def mkdir(name: String){
+
+  def mkdir(name: String) {
     //死んでる変数ですが、どうやっても""の中に変数を入れる方法が見つからなかったので、一旦stringに格納
-    val gitPlainText = "git -C ../before/src/"+name+" remote -v"
-    val gitText = Process(Seq("sh","-c",gitPlainText)).lines.toList(0)
+    //val gitText = Process(Seq("sh","-c","git -C ../before/src/"+name+" remote -v")).lines.toList(0)
+    val gitText: String = Process(Seq("sh", "-c", "git -C ../before/src/" + name + " remote -v")).lines.head.dropRight(8)
+
+    //"project5.git (fetch)" or "project4 (fetch)"と2パターンあったのでifで処理
+    val collectedGitText: String =
+      if (gitText.endsWith(".git")) {
+        gitText.dropRight(4)
+      } else {
+        gitText
+      }
 
     //リポジトリ名を先に記しておかないといけない仕様
     val githubWith = "(.*github.com.*)".r
     val googleWith = "(.*source.developers.google.com.*)".r
-    val awsWith = "(.*git-codecommit.us-east-1.amazonaws.com.*)".r
+    val awsWith    = "(.*git-codecommit.us-east-1.amazonaws.com.*)".r
 
-      gitText match {
-        case githubWith(matched) => 
-         gitText.split("github.com.").last.split("/").init.foreach()
-       
-        case _ =>
-      }
-      gitText match {
-        case googleWith(matched) => 
-          //println(gitText.split("source.developers.google.com.")(1))
-        case _ =>
-      }
-      gitText match {
-        case awsWith(matched) =>
-          //println(gitText.split("git-codecommit.us-east-1.amazonaws.com.")(1))
-        case _ =>
-      }
-
+    collectedGitText match {
+      case githubWith(matched) =>
+        val dirName: String = collectedGitText.split("github.com.").last.split("/").toSeq.mkString("/")
+        println(dirName)
+        Process(Seq("sh", "-c", "mkdir -p ../before/src/github.com/" + dirName)).run
+      case _ =>
+    }
+    collectedGitText match {
+      case googleWith(matched) =>
+        val dirName: String = collectedGitText.split("source.developers.google.com.").last.split("/").toSeq.mkString("/")
+        println(dirName)
+        Process(Seq("sh", "-c", "mkdir -p ../before/src/source.developers.google.com/" + dirName)).run
+      case _ =>
+    }
+    collectedGitText match {
+      case awsWith(matched) =>
+        val dirName: String = collectedGitText.split("git-codecommit.us-east-1.amazonaws.com.").last.split("/").toSeq.mkString("/")
+        println(dirName)
+        Process(Seq("sh", "-c", "mkdir -p ../before/src/git-codecommit.us-east-1.amazonaws.com./" + dirName)).run
+      case _ =>
+    }
   }
 }
